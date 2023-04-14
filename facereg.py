c8c12dd7bc20c3dd124abdb16041053c9ca7f6c8
amanda <100282290+amanda-lais@users.noreply.github.com>
2023-04-13 23:08:03 -0300

    Update facereg.py

--- facereg.py
+++ facereg.py
@@ -3,8 +3,8 @@ import face_recognition
 import numpy as np
 import cv2
 
-# !! Esse código vai para o comunicacao.py eventualmente, mas enquanto não se resolve o problema do serial quero deixar aqui
-# para evitar que fique MUITO bagunçado
+# [!!] Esse código vai para o comunicacao.py eventualmente, mas enquanto não se resolve o problema do serial quero deixar aqui [!!]
+# para evitar que fique MUITO bagunçado e para tudo ser resolvido passo a passo em vez de ficar caótico
 
 video_capture = cv2.VideoCapture(0)
 face_locations = []
@@ -21,7 +21,7 @@ def pega_localizacoes(video_capture, face_locations, face_encodings, process_thi
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
         process_this_frame = not process_this_frame
         for (top, right, bottom, left) in face_locations:
-            # !! eu sei que não faz sentido o return aqui, mas por *algum* motivo buga se tento fora desse for
+            # [!!] eu sei que não faz sentido o return aqui, mas por *algum* motivo buga se tento fora desse for [!!]
             return(face_locations)
             # vv não sei se é relevante no nosso caso
             # Scale back up face locations since the frame we detected in was scaled to 1/4 size
@@ -36,5 +36,6 @@ def pega_localizacoes(video_capture, face_locations, face_encodings, process_thi
 
 while True:
   # print é só para testes, eu juntar comunicacao.py com esse faço a integração certinha
-  # !! dependendo de como é feito, a leitura dos quadrantes fica EXTREMAMENTE lenta, esse jeito do código ainda tá numa velocidade ok porém
+  # [!!] dependendo de como é feito, a leitura dos quadrantes fica EXTREMAMENTE lenta [!!]
+  # esse jeito do código ainda está numa velocidade ok porém
   print(pega_localizacoes(video_capture, face_locations, face_encodings, process_this_frame))
