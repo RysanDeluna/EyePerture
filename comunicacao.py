b7a11492f6a3b502f097056a7c43e0dc67bc79a8
amanda <100282290+amanda-lais@users.noreply.github.com>
2023-04-15 15:02:24 -0300

    Update comunicacao.py

--- comunicacao.py
+++ comunicacao.py
@@ -6,18 +6,24 @@ import keyboard
 
 # [!!] PARA RODAR NO LINUX:
 # aport = serial.Serial('/dev/ttyUSB0', 9600, timeout=1, dsrdtr=True)
+
+# Get a reference to webcam #0 (the default one)
+# Para corrigir o problema do loop, dsrdtr foi configurada como True:
+### dsrdtr (bool) – Enable hardware (DSR/DTR) flow control.
 aport = serial.Serial("COM3", 9600, timeout=1, dsrdtr=True)
 
+# Função temporária apenas para a prova de conceito que registra
+# os dígitos de 1-4 quando pressionados no teclado, representando os quadrantes.
+# Mais tarde, uma função enviará esses mesmos dígitos, mas em vez de vir do teclado,
+# virão do reconhecimento facial.
 def identif_quad() -> int:
     kb = keyboard.read_key()
-    while kb!='1' and kb!='2' and kb!='3' and kb!='4': # não modifiquei para o que a gente 
-        kb = keyboard.read_key()                       # fez em sala pois poderia gerar outros problemas
-    # futuramente ira retornar o quadrante
-    # do rosto, identificado com o uso de APIs
+    while kb!='1' and kb!='2' and kb!='3' and kb!='4':
+        kb = keyboard.read_key()
     return int(kb)
 
 while aport:
     quadrante = bytes(identif_quad())
-    #print(quadrante)
+    #print(quadrante) #para testes
     aport.write(quadrante)
     aport.read()
