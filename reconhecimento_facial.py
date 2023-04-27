ecd0ddf855a4e1b84a34dbcd31d9589094525710
Ryan Marco <ryan.marco.ismart@gmail.com>
2023-04-27 13:51:45 -0300

    Nova logica e adicao de visualizacao para testes

--- reconhecimento_facial.py
+++ reconhecimento_facial.py
@@ -1,55 +1,50 @@
 import face_recognition
 import numpy as np
 import cv2
+import serial
 
-# [!!] Esse código vai para o comunicacao.py eventualmente, mas enquanto não se resolve o problema do serial quero deixar aqui [!!]
-# para evitar que fique MUITO bagunçado e para tudo ser resolvido passo a passo em vez de ficar caótico
+def encontra_rosto(video_capture, PROCESS_FRAME):
+    face_locations = []
+    ret, frame = video_capture.read()
+    if PROCESS_FRAME == True:
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+    PROCESS_FRAME = not PROCESS_FRAME
+    
+    # Mostra a imagem de vídeo com um retângulo --> Testing only!
+    for (top, right, bottom, left) in face_locations:
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+    
+        cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0,0,255), cv2.FILLED)
+    
+    cv2.imshow('Video', frame)
+    return face_locations, PROCESS_FRAME, small_frame.shape
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-# Initialize some variables
-face_locations = []
-face_encodings = []
-process_this_frame = True
+def identifica_quadrantes(face_locations, imgs_shape):
+    x = face_locations[3] - face_locations[1]
+    y = face_locations[2] - face_locations[0]
 
-# Função que identifica as localizações do rosto que está sendo capturado pela webcam no momento
-# e as devolve dentro uma tupla. Dentro dela, faz algumas coisas para reduzir o tempo de processamento da imagem.
-def pega_localizacoes(video_capture, face_locations, face_encodings, process_this_frame):
-    while True:
-        # Grab a single frame of video
-        ret, frame = video_capture.read()
-        # Only process every other frame of video to save time
-        if process_this_frame:
-             # Resize frame of video to 1/4 size for faster face recognition processing
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_small_frame = small_frame[:, :, ::-1]
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            #face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations) #remover faz o código rodar mais rápido
-        process_this_frame = not process_this_frame
-        # Normalmente o for seguinte na função que usei como referência faria o isso:
-        ### Display the results (não é relevante para o nosso caso)
-        # Entretanto, no momento está contido nela o return da função.
-        # [!!] Eu sei e concordo que não faz sentido o return aqui, mas por *algum* motivo buga se tento fora dele,
-        # então, *por enquanto* fica aqui mesmo.
-        return face_locations
+    
 
-    # Para o nosso projeto não é relevante, mas deixei para evitar chance do código quebrar
-    # Release handle to the webcam
+if __name__ == "__main__":
+    aport = serial.Serial('/dev/ttyACM0', 9600, timeout=1, dsrdtr=True)
+    video_capture = cv2.VideoCapture(0)
+    PROCESS_FRAME = True
+
+    while True: 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        face_locations, PROCESS_FRAME, shape = encontra_rosto(video_capture, PROCESS_FRAME)
+    
+    
+    
+
+
     video_capture.release()
     cv2.destroyAllWindows()
-
-
-
-while True:  
-  print(pega_localizacoes(video_capture, face_locations, face_encodings, process_this_frame)) #print para teste
-
-# [!!] No momento está ok, mas dependendo de como é feito o código, a leitura dos quadrantes fica lenta
-# [!!] Eu removi enquanto limpando esse código, mas caso necessário para a prova de conceito posso colocar de novo
-# um trecho do código que mostra a imagem sendo capturada e opcionalmente um frame em volta do rosto detectado.
-# Minhas referências:
-### https://github.com/ageitgey/face_recognition/blob/master/examples/facerec_from_webcam_faster.py
-### https://face-recognition.readthedocs.io/en/latest/face_recognition.html
-# face-recognition package: https://pypi.org/project/opencv-python/      [ pip install opencv-python    ]
-# opencv:                   https://pypi.org/project/face-recognition/   [ pip install face-recognition ]
